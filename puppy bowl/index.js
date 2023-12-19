a3db49b563024a13d7ac12b0ef89e8ff891af42c
Porter Peterson <porterpeterson@Porters-MacBook-Air.local>
2023-12-19 10:26:07 -0700

    first commit

---
+++ puppy bowl/index.js
@@ -0,0 +1,48 @@
+const span = document.querySelector("span");
+const ul = document.querySelector("ul");
+let players = [];
+
+window.addEventListener("hashchange", function () {
+  render();
+});
+
+function render() {
+  console.log(players);
+  span.innerHTML = players.length;
+  const hash = window.location.hash;
+  const id = hash.slice(1) * 1;
+  let filtered = players;
+  if (id) {
+    filtered = filtered.filter(function (player) {
+      return player.id === id;
+    });
+  }
+  const html = filtered
+    .map(function (player) {
+      return `
+      <li>
+        <h4><a href='#${player.id}'>${player.name}</a></h4>
+        <p>
+          ${player.breed}
+        </p>
+        
+      </li>
+    `;
+    })
+    .join("");
+  ul.innerHTML = html;
+}
+
+render();
+
+async function fetchPlayers() {
+  const response = await fetch(
+    "https://fsa-puppy-bowl.herokuapp.com/api/2309-ftb-et-am/players"
+  );
+  const json = await response.json();
+  console.log(json);
+  players = json.data.players;
+  render();
+}
+
+fetchPlayers();
